2b5aedf6d7785f5b4fe5f719532869d549c4581c
vijaykarthik <nvijaykarthik@gmail.com>
2018-04-17 10:54:21 +0530

    initial commit

---
+++ src/main/java/io/broko/aratai/ArataiApplication.java
@@ -0,0 +1,12 @@
+package io.broko.aratai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArataiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArataiApplication.class, args);
+	}
+}
---
+++ src/test/java/io/broko/aratai/ArataiApplicationTests.java
@@ -0,0 +1,16 @@
+package io.broko.aratai;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArataiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
